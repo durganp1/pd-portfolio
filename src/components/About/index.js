29d542a4c7a7e46d56b18aabe09ddbed2dd966ce
durganp1 <durganp1@gmail.com>
2020-11-02 15:35:03 -0600

    added About component and data

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,7 +7,16 @@ function About() {
     return (
         <section>
             <h1>Peter J Durgan</h1>
-            <p>Short Bio</p>
+            <p>
+                    I have lived in the Chicago area for over 15 years with my wife
+                and 2 children.  My career has focused on Hospitality and Food Service 
+                Management for the past 25 years.  This has lead me through multiple 
+                companies, while growing up to my most recent position as District Manger
+                for the Midwest.  At this point in my career, I am looking to enter into the 
+                field of web development.  It is my goal to use my prior experince combined 
+                with my new found experince of coding.
+                    
+            </p>
         </section>
     );
 }
